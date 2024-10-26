5f0d141b975267e59cad168b3b627ae578573c64
fenroe <scrambles9494@gmail.com>
2024-10-26 19:18:59 +0300

    Initial commit

---
+++ client/go.mod
@@ -0,0 +1,3 @@
+module github.com/Fenroe/carbonara
+
+go 1.23.1
---
+++ server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler) // Set route for root path
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
