f30d67c3be16b330d09f1264459c83e82e50717f
fenroe <scrambles9494@gmail.com>
2024-10-26 20:29:08 +0300

    reading from clipboard

--- client/main.go
+++ client/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"golang.design/x/clipboard"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println("Reading your clipboard...")
+	err := clipboard.Init()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	text := string(clipboard.Read(clipboard.FmtText))
+	if text != "" {
+		fmt.Println(text)
+		return
+	}
+	image := clipboard.Read(clipboard.FmtImage)
+	fmt.Println(image)
+	if image != nil {
+		fmt.Println("You have an image")
+		return
+	}
 }
